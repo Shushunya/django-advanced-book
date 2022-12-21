58aa48417b80207779518886ea3368d21e9bea9c
Oleksandra <mremarka314@gmail.com>
2022-12-22 00:01:11 +0200

    simple login/logout/signup added

--- books/books/urls.py
+++ books/books/urls.py
@@ -17,6 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
-    path('', include('pages.urls'),)
+
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')),
+    
+    # local apps
+    path('', include('pages.urls'),),
+    path('accounts/', include('accounts.urls')),
 ]
